666deaa509b16edcda694762f8331d83a830babe
sabita <sabitakhadka091@gmail.com>
2024-04-11 17:07:52 +0545

    implement Contact,FeatureSection,FeaturePresentation,PipitBenefitsTable,Banners

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import React from "react";
+
 import HomePage from "./components/HomePage";
+import Banners from "./components/Banners";
+import Contact from "./components/Contact";
+import PipitBenefitsTable from "./components/PipitBenefitsTable";
+import FeaturePresentation from "./components/FeaturePresentation";
+import FeatureSection from "./components/FeatureSection";
 
 const App: React.FC = () => {
   return (
     <>
       <HomePage />
+      <FeatureSection />
+      <FeaturePresentation />
+      <PipitBenefitsTable />
+      <Banners />
+      <Contact />
     </>
   );
 };
