69258dda3ba89ac0050dd85315427f9e0ee60c2d
sabita <sabitakhadka091@gmail.com>
2024-04-11 15:16:20 +0545

    implement the contact

--- src/components/Banners.tsx
+++ src/components/Banners.tsx
@@ -9,29 +9,33 @@ import Banner2 from "./assets/image 16.svg";
 import Banner3 from "./assets/image 17.svg";
 //@ts-ignore
 import Banner4 from "./assets/image 18.svg";
+import Contact from "./Contact";
 
 function Banners() {
   return (
-    <Box mt={100} h={"328px"}>
-      <Title
-        fw={700}
-        size={"40px"}
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          lineHeight: "48px",
-        }}
-        c={"#222A4E"}
-      >
-        Our Partners
-      </Title>
-      <Flex gap={"80px"} align={"center"} justify={"center"} mt={60}>
-        <Image src={Banner1} />
-        <Image src={Banner2} />
-        <Image src={Banner3} />
-        <Image src={Banner4} />
-      </Flex>
-    </Box>
+    <>
+      <Box mt={100} h={"328px"}>
+        <Title
+          fw={700}
+          size={"40px"}
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            lineHeight: "48px",
+          }}
+          c={"#222A4E"}
+        >
+          Our Partners
+        </Title>
+        <Flex gap={"80px"} align={"center"} justify={"center"} mt={60}>
+          <Image src={Banner1} />
+          <Image src={Banner2} />
+          <Image src={Banner3} />
+          <Image src={Banner4} />
+        </Flex>
+      </Box>
+      <Contact />
+    </>
   );
 }
 
