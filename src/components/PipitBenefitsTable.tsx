797fde97e45fc8497bbad24a9114d082f5fb5e0f
sabita <sabitakhadka091@gmail.com>
2024-04-11 13:54:44 +0545

    feat: Implement PipitBenefitsTable to showcase customer segments

--- src/components/PipitBenefitsTable.tsx
+++ src/components/PipitBenefitsTable.tsx
@@ -1,5 +1,46 @@
+import { Box, Text } from "@mantine/core";
+
 function PipitBenefitsTable() {
-  return <div>PipitBenefitsTable</div>;
+  return (
+    <Box bg="#CCF0F133" mt={130} w={"1,440px"} h={"668px"}>
+      <Box
+        style={{
+          lineHeight: "48px",
+          display: "grid",
+          font: "Proxima Nova",
+          justifyContent: "center",
+        }}
+      >
+        <Text
+          mt={115}
+          size={"40px"}
+          fw={700}
+          c="#222A4E"
+          style={{
+            lineHeight: "48px",
+            display: "flex",
+            justifyContent: "center",
+            letterSpacing: "1px",
+            font: "Proxima Nova",
+          }}
+        >
+          Who Can Benefit From Pipit?
+        </Text>
+        <Text
+          mt={20}
+          fw={400}
+          size="20px"
+          c={"#474E6C"}
+          style={{
+            lineHeight: "28px",
+            font: "Proxima Nova",
+          }}
+        >
+          Delivering a hassle-free experience for both you and your customers.
+        </Text>
+      </Box>
+    </Box>
+  );
 }
 
 export default PipitBenefitsTable;
