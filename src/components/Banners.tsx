83757f22a2d5bfc3b3a6c01c1bb6bf04b1bbac53
sabita <sabitakhadka091@gmail.com>
2024-04-11 15:05:55 +0545

    Create a Banners component using Flex and Box from @mantine/core

--- src/components/Banners.tsx
+++ src/components/Banners.tsx
@@ -1,5 +1,38 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import { Box, Flex, Image, Title } from "@mantine/core";
+
+//@ts-ignore
+import Banner1 from "./assets/image 15.svg";
+//@ts-ignore
+import Banner2 from "./assets/image 16.svg";
+//@ts-ignore
+import Banner3 from "./assets/image 17.svg";
+//@ts-ignore
+import Banner4 from "./assets/image 18.svg";
+
 function Banners() {
-  return <div>Banners</div>;
+  return (
+    <Box mt={100} h={"328px"}>
+      <Title
+        fw={700}
+        size={"40px"}
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          lineHeight: "48px",
+        }}
+        c={"#222A4E"}
+      >
+        Our Partners
+      </Title>
+      <Flex gap={"80px"} align={"center"} justify={"center"} mt={60}>
+        <Image src={Banner1} />
+        <Image src={Banner2} />
+        <Image src={Banner3} />
+        <Image src={Banner4} />
+      </Flex>
+    </Box>
+  );
 }
 
 export default Banners;
