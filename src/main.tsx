1eceeb347ad05b862a399995ae971a17f2e2a6a8
sabita <sabitakhadka091@gmail.com>
2024-04-14 20:27:20 +0545

    implement the index.css file

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "@mantine/core/styles.css";
 import { MantineProvider } from "@mantine/core";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
