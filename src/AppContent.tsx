4b19eb7c0493f59019f8d6460a7c3b1009269243
sabita <sabitakhadka091@gmail.com>
2024-04-17 10:00:32 +0545

    created new component for route

--- src/AppContent.tsx
+++ src/AppContent.tsx
@@ -1,5 +1,22 @@
-function AppContent() {
-  return <div>AppContent</div>;
-}
+import React from "react";
+import HomePage from "./components/HomePage";
+import Banners from "./components/Banners";
+import Contact from "./components/Contact";
+import PipitBenefitsTable from "./components/PipitBenefitsTable";
+import FeaturePresentation from "./components/FeaturePresentation";
+import FeatureSection from "./components/FeatureSection";
+
+const AppContent: React.FC = () => {
+  return (
+    <>
+      <HomePage />
+      <FeatureSection />
+      <FeaturePresentation />
+      <PipitBenefitsTable />
+      <Banners />
+      <Contact />
+    </>
+  );
+};
 
 export default AppContent;
