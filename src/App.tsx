cdd09019fa0cb41390d49cf88c8cbaf154c6e0fb
sabita <sabitakhadka091@gmail.com>
2024-04-17 10:02:10 +0545

    remove all import and add only one import appcontent

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,11 @@
 import React from "react";
-import HomePage from "./components/HomePage";
-import Banners from "./components/Banners";
-import Contact from "./components/Contact";
-import PipitBenefitsTable from "./components/PipitBenefitsTable";
-import FeaturePresentation from "./components/FeaturePresentation";
-import FeatureSection from "./components/FeatureSection";
+
+import AppContent from "./AppContent";
 
 const App: React.FC = () => {
   return (
     <>
-      <HomePage />
-      <FeatureSection />
-      <FeaturePresentation />
-      <PipitBenefitsTable />
-      <Banners />
-      <Contact />
+      <AppContent />
     </>
   );
 };
