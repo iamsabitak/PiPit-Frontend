168b233fda8a5176bf68a4934fba9d7693d39486
sabita <sabitakhadka091@gmail.com>
2024-04-17 15:54:46 +0545

    remove unnecessary code

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -7,50 +7,9 @@ import { Box } from "@mantine/core";
 function HomePage() {
   return (
     <>
-      {/* <Box
-        bg={"#ffff"}
-        // mt={-60}
-        // pl={40}
-        // ml={-50}
-        w={"305px"}
-        className={`dropdown-menu ${isMenuOpen ? "active" : ""}`}
-        style={{ display: isMenuOpen ? "grid" : "none", zIndex: "-1" }}
-      >
-        <Anchor href="#" style={{ textDecoration: "none" }} onClick={hideMenu}>
-          <Text
-            style={{ lineHeight: "22.4px" }}
-            fw={600}
-            size="16px"
-            c={"#222A4E"}
-          >
-            What can you get?
-          </Text>
-        </Anchor>
-        <Anchor href="#" style={{ textDecoration: "none" }} onClick={hideMenu}>
-          <Text
-            style={{ lineHeight: "22.4px" }}
-            fw={600}
-            size="16px"
-            c={"#222A4E"}
-          >
-            Benefits
-          </Text>
-        </Anchor>
-        <Anchor href="#" style={{ textDecoration: "none" }} onClick={hideMenu}>
-          <Text
-            style={{ lineHeight: "22.4px" }}
-            fw={600}
-            size="16px"
-            c={"#222A4E"}
-          >
-            Contact
-          </Text>
-        </Anchor>
-      </Box> */}
       <Box
         className="container"
         w={"1,440px"}
-        // h={"750px"}
         bg={
           "linear-gradient(90deg, rgba(219,249,255,1) 0%, rgba(181,242,255,1) 55%, rgba(155,175,242,1) 140%)"
         }
